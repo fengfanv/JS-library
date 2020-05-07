3f2d6ec8a1dba462597bf850bfdcfd4539ffafc6
kangyupeng <1431826670@qq.com>
2020-05-07 11:16:12 +0800

    更新_node_文源码文件上传案案例_不能处理中文的bug

--- node/YUMA_uploadFiles/index.js
+++ node/YUMA_uploadFiles/index.js
@@ -14,19 +14,29 @@ http.createServer(function (request, response) {
 	fs.stat(file_address, function (err, stat) {
 		if (err) {
 			if (pathname === '/api/uploadFiles') {
-				request.setEncoding('binary'); //设置接收数据的编码格式，这数据不能强转utf8，转后在转回来就用不了了，解决formdata数据乱码问题
+				//request.setEncoding('binary'); //设置接收数据的编码格式，这数据不能强转utf8，转后在转回来就用不了了，解决formdata数据不能用问题
+				request.setEncoding('base64');
+				
 				var postData = '';
 				request.on('data', function (chunk) {
 					//用postData += chunk这种方式接收数据有1gb左右的限制，
 					postData += chunk;
 				});
 				request.on('end', async function () {
-					response.writeHead(200, {
-						'Content-Type': 'text/plain'
-					});
+					response.writeHead(200, {'Content-Type': 'text/json'});
 					//console.log(postData);//这里就是浏览器表单或formdata处理后发过来的数据最原始状态
-					var dataBody = await jiexiFormData(postData);//解析formdata数据
+					//var dataBody = await jiexiFormData(postData);//解析formdata数据
+					//response.end(JSON.stringify(dataBody));
+
+					var dataBody = await parseFormData(postData);//解析formdata数据
+					console.log(dataBody);
 					response.end(JSON.stringify(dataBody));
+
+					// console.log(postData);
+					// console.log(1111111111111111111);
+					// console.log(Buffer.from(postData, 'base64').toString('binary'));
+
+					//response.end('Has received send data!');
 				});
 			} else {
 				response.writeHead(404, {
@@ -65,10 +75,183 @@ http.createServer(function (request, response) {
 console.log('服务地址', 80);
 
 //解析formdata数据
+function parseFormData(formdata) {
+	return new Promise(function (resolve, reject) {
+
+		//2020年5月6日通过接收编码为base64的数据在转换成binary编码数据实现文件上传
+		var postData = Buffer.from(formdata, 'base64').toString('binary');//将编码为base64的字符串转换为buffer实体再转换为编码为binary的字符串
+
+		var postData_utf8 = Buffer.from(formdata, 'base64').toString();
+
+		//.replace(/^\s\s*/, '')匹配空格
+		//1、找到处理后生成的hexname值，已便定位formdata处理过的文件数据和其它参数在那个位置
+		//hexname值作用，用来标记数据位置（这个其实叫边界字符串）
+		//找上面那个hexname值技巧，从尾部找找到\r\n停止
+		var postDataLen = postData.length;
+		var i = postDataLen;
+		var foundValue = '';
+		//console.log('开始找hexname值');
+		while (foundValue !== '\r\n--') {
+			//console.log(foundValue);
+			//console.log(i);
+			i--;
+			foundValue = postData.substr(i, 4);
+		}
+		var hexname = postData.substring(i, postDataLen).replace(/[\r\n]/g, "");
+		hexname = hexname.substring(0, hexname.length - 2);
+		//console.log(hexname);
+		//至此hexname输出的后16为，为hex值
+
+
+		//2、根据上面找的hexname值把每一个formdata中每个参数切割，分出来
+
+		//处理编码为binary
+		var Arr1 = postData.split(hexname + '\r\n');
+		var dataArr = Arr1.slice(1, Arr1.length);
+
+		//处理编码为utf8
+		var Arr1_utf8 = postData_utf8.split(hexname + '\r\n');
+		var dataArr_utf8 = Arr1_utf8.slice(1, Arr1_utf8.length);
+
+		//通过hexname切割并把formdata中的参数转到数组后，在过滤一下，防止在不上传文件时，防止切割后的参数体中还有hexname的情况
+		dataArr.forEach(function(item,index){
+			//处理编码为binary
+			dataArr[index] = item.replace(hexname, "");
+
+			//处理编码为utf8
+			dataArr_utf8[index] = dataArr_utf8[index].replace(hexname, "");
+		});
+		var dataArrLen = dataArr.length;
+
+
+		//3、分析数据，把formdata中每一个参数的数据状态信息(数据名称,数据类型什么的)和数据本体提取出来
+		var dataBody = {}; //dataBody用来存放处理后formdata中每一个参数状态信息和数据体的容器
+		console.log('FormData 参数数量：' + dataArrLen);
+		function eachArr(index){
+			//item现在是formdata中某一个参数的数据状态信息(数据名称,数据类型什么的)和数据值本身的结合体
+			let item = dataArr[index];
+			let addIndex = index+1;
+
+			//处理编码为binary
+			let valueData = item;
+
+			//处理编码为utf8
+			let valueData_utf8 = dataArr_utf8[index];
+
+			//寻找formdata中某一个参数的数据状态信息(数据名称,数据类型什么的)和数据本体中间的位置
+			let endzuobiao = valueData.search('\r\n\r\n');
+
+			//3.1、根据endzuobiao坐标之前为数据状态信息(数据名称,数据类型什么的)（3.1、提取参数的数据状态信息）
+			//console.log(valueData.substring(0, endzuobiao));
+
+			//把参数的数据状态信息字符串转换成arr，因为一个参数的状态信息不光有数据名称,还有别的数据说明属性，所以要把它换成arr
+			//处理编码为binary
+			let paramArr = valueData.substring(0, endzuobiao).replace(/[\r\n]/g, ";").split(';');
+
+			//处理编码为utf8
+			let paramArr_utf8 = valueData_utf8.substring(0, endzuobiao).replace(/[\r\n]/g, ";").split(';');
+
+			//把参数的数据状态的信息提分别提取到对象中
+			//提取参数 数据状态信息用utf8编码的，这样可以防止上传文件时，如果文件名称是中文的话无法识别中文的问题
+			let param = {};//用于临时存放某个已处理好参数（已处理好的数据状态信息和对象和数据体）的容器
+			paramArr.forEach(function (paramArrItem, paramArrIndex) {
+				if (paramArrItem.length > 0) {
+					var fuhao = ":";
+					if (paramArrItem.search(":") !== -1) {
+						fuhao = ":";
+					} else {
+						fuhao = "=";
+					}
+					//处理编码为binary
+					// var paramArrItemkey = paramArrItem.split(fuhao)[0].replace(/\ +/g, "").replace(/['"]/g, "");
+					// var paramArrItemvalue = paramArrItem.split(fuhao)[1].replace(/\ +/g, "").replace(/['"]/g, "");
+
+					//处理编码为utf8
+					var paramArrItemkey = paramArr_utf8[paramArrIndex].split(fuhao)[0].replace(/\ +/g, "").replace(/['"]/g, "");
+					var paramArrItemvalue = paramArr_utf8[paramArrIndex].split(fuhao)[1].replace(/\ +/g, "").replace(/['"]/g, "");
+					param[paramArrItemkey] = paramArrItemvalue;
+				}
+			});
+
+
+			//3.2、根据endzuobiao坐标之后为数据本体（3.2、提取参数的数据本体）
+			//根据提取的数据状态信息，分别用不同编码方式的数据来提取数据体
+			if (param['Content-Type'] !== undefined) {
+				//提取文件类数据体，文件类数据用binary编码的，防止数据写入文件后，文件无法使用
+				//处理编码为binary
+				let dataBody = valueData.substring(endzuobiao);
+				let data = dataBody.substring(4, dataBody.length - 4); //去除数据头尾的换行符
+
+				//console.log('这应该是图片类数据');
+				/*
+				.png 正常解析保存
+				.jpg 正常解析保存
+				.mp4 正常解析保存
+				.mp3 正常解析保存
+				.txt 正常解析保存，txt内容有中文也会正常保存，
+				.word 正常保存，但是打开时说无法正常读取文件，需要修复，修复后可正常打开
+				*/
+				//把图片类数据写入文件
+				let filename = Date.now() + param.filename.replace(/['"]/g, "");
+				//创建可写为流文件
+				let writerStream = fs.createWriteStream(filename);
+				writerStream.write(data, 'binary');
+				// 标记文件末尾
+				writerStream.end();
+				// 处理流事件 ==> finish 事件
+				writerStream.on('finish', function () {
+					//finish所有数据已被写入到底层系统时触发。
+					console.log(param.filename + '_写入完成');
+					param.data = filename; //图片类的文件写入文件地址
+					if(addIndex!==dataArrLen){
+						eachArr(addIndex);
+					}
+				});
+				writerStream.on('error', function (err) {
+					console.log(param.filename + '_写入失败');
+					//console.log(err.stack);
+					if(addIndex!==dataArrLen){
+						eachArr(addIndex);
+					}
+				});
+			} else {
+				//提取普通数据体，普通数据用utf8编码的，防止出现中文字无法识别
+				//处理编码为utf8
+				let dataBody_utf8 = valueData_utf8.substring(endzuobiao);
+				let data_utf8 = dataBody_utf8.replace(/[\r\n]/g, "").replace(/--$/, ""); //除去空格和结尾的--
+				//普通数据,字符串型数据，啥的，object型数据会在浏览器上formdata解析成[object object]所以obj在传输前要转换成字符串
+
+				//处理编码为utf8,直接将数据存入
+				param.data = data_utf8; //将处理好的普通数据体写入到param中
+				if(addIndex!==dataArrLen){
+					eachArr(addIndex);
+				}
+			}
+			dataBody[param.name] = param; //将这个整理好的formdata参数存入到dataBody中
+
+			if (index === dataArrLen - 1) {
+				//formData中所有参数处理完成
+				resolve(dataBody);
+			};
+		};
+		eachArr(0);
+	});
+}
+
+
+
+//-------------------------------------------------------------------------------------------------------------------------------
+
+//解析编码为binary 的 formdata数据
 function jiexiFormData(formdata) {
 	return new Promise(function (resolve, reject) {
 		//console.log(formdata);
-		var postData = formdata;
+		//var postData = formdata;
+
+		//2020年5月6日通过接收编码为base64的数据在转换成binary编码数据实现文件上传
+		
+		var postData = Buffer.from(formdata, 'base64').toString('binary');
+
 		//.replace(/^\s\s*/, '')匹配空格
 		//1、找到处理后生成的hex值，已便定位formdata处理过的文件数据和其它数据在发送过来的那个位置
 		//hex值作用，用来标记数据位置（这个其实叫边界字符串）
@@ -91,6 +274,12 @@ function jiexiFormData(formdata) {
 		//2、根据上面找的hex值把数据切割，分出来
 		var Arr1 = postData.split(hexname + '\r\n');
 		var dataArr = Arr1.slice(1, Arr1.length);
+
+		//数据通过hexname切割完毕并转换成数组后，在过滤一下，防止在不上传文件时，参数体内还有hexname的情况
+		dataArr.forEach(function(item,index){
+			dataArr[index] = item.replace(hexname, "");
+		});
+
 		var dataArrLen = dataArr.length;
 
 		//console.log(dataArr);
@@ -104,7 +293,9 @@ function jiexiFormData(formdata) {
 			let addIndex = index+1;
 
 			//item现在是数据状态信息（数据名称）和数据值本身的结合体，
-			var valueData = item.toString();
+			var valueData = item;
+
+			console.log(valueData);
 			var endzuobiao = valueData.search('\r\n\r\n');
 
 			//3.1、根据endzuobiao坐标之前为数据状态信息(数据名称什么的)
@@ -132,6 +323,9 @@ function jiexiFormData(formdata) {
 			//console.log('start数据本体');
 			var data1 = valueData.substring(endzuobiao);
 			var data = data1.substring(4, data1.length - 4); //去除数据头尾的换行符
+
+
+
 			//console.log(data);
 			//console.log('end数据本体');
 			if (param['Content-Type'] !== undefined) {
@@ -171,6 +365,7 @@ function jiexiFormData(formdata) {
 				//普通数据,字符串型数据，啥的，object型数据会被formdata解析成[object object]所以obj要转换成字符串
 				//如果普通数据中有中文，则会在接收数据时解析成乱码
 				let dataStep1 = data.replace(hexname, ""); //剔除尾部hex标识
+				console.log('本次的hexname:'+hexname);
 				let dataStep2 = dataStep1.replace(/[\r\n]/g, ""); //剔除换行符
 				param.data = dataStep2; //普通数据写入数据本体
 				if(addIndex!==dataArrLen){
